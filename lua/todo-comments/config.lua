5ae5e0bae532a501ad29b882a1837e3c431a098a
McAuleyPenney <59481467+McAuleyPenney@users.noreply.github.com>
2021-05-29 14:56:15 -0700

    Update config.lua
    "FIX" listed as an alternative to itself

--- lua/todo-comments/config.lua
+++ lua/todo-comments/config.lua
@@ -19,7 +19,7 @@ local defaults = {
     FIX = {
       icon = " ", -- icon used for the sign, and in search results
       color = "error", -- can be a hex color, or a named color (see below)
-      alt = { "FIXME", "BUG", "FIXIT", "FIX", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+      alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
       -- signs = false, -- configure signs for some keywords individually
     },
     TODO = { icon = " ", color = "info" },
