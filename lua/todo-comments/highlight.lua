98b1ebf198836bdc226c0562b9f906584e6c400e
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-01-19 15:18:53 +0100

    fix: properly clear todo highlights in stop

--- lua/todo-comments/highlight.lua
+++ lua/todo-comments/highlight.lua
@@ -274,7 +274,7 @@ function M.stop()
   pcall(vim.cmd, "augroup! Todo")
   M.wins = {}
   vim.fn.sign_unplace("todo-signs")
-  for _, buf in pairs(M.bufs) do
+  for buf, _ in pairs(M.bufs) do
     vim.api.nvim_buf_clear_namespace(buf, Config.ns, 0, -1)
   end
   M.bufs = {}
