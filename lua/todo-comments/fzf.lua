001437b27fe51bf4398293b9e33bf57a98f102b8
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-06-10 18:11:55 +0200

    chore: cleanup

--- lua/todo-comments/fzf.lua
+++ lua/todo-comments/fzf.lua
@@ -1,19 +1,18 @@
 local Config = require("todo-comments.config")
 local Grep = require("fzf-lua.providers.grep")
-local utils = require("fzf-lua.utils")
 
 local M = {}
 
----@param opts_keywords? string|string[]
-local function keywords_filter(opts_keywords)
-  local all_keywords = vim.tbl_keys(Config.keywords)
-  if not opts_keywords then
-    return all_keywords
+---@param filter? string|string[]
+local function keywords_filter(filter)
+  local all = vim.tbl_keys(Config.keywords)
+  if not filter then
+    return all
   end
-  local filters = type(opts_keywords) == "string" and { opts_keywords } or opts_keywords
+  local filters = type(filter) == "string" and { filter } or filter
   return vim.tbl_filter(function(kw)
     return vim.tbl_contains(filters, kw)
-  end, all_keywords)
+  end, all)
 end
 
 ---@param opts? {keywords: string[]}
@@ -22,8 +21,6 @@ function M.todo(opts)
     opts = {}
   end
   opts.no_esc = true
-  -- match whole words only (#968)
-  opts.search = [[\b]] .. utils.rg_escape(vim.fn.expand("<cword>")) .. [[\b]]
   opts.search = Config.search_regex(keywords_filter(opts.keywords))
   return Grep.grep(opts)
 end
