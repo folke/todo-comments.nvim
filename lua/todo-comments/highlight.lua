88d9564af85c08ac132ff4c047599b19fc6f65ae
ngpong <pengpeng1997428@gmail.com>
2024-07-06 03:46:50 +0800

    feat: compatible with Trouble v3.

--- lua/todo-comments/highlight.lua
+++ lua/todo-comments/highlight.lua
@@ -387,7 +387,6 @@ function M.start()
     [[augroup Todo
         autocmd!
         autocmd BufWinEnter,WinNew * lua require("todo-comments.highlight").attach()
-        autocmd BufWritePost * silent! lua require'trouble'.refresh({auto = true, provider = "todo"})
         autocmd WinScrolled * lua require("todo-comments.highlight").highlight_win()
         autocmd ColorScheme * lua vim.defer_fn(require("todo-comments.config").colors, 10)
       augroup end]],
