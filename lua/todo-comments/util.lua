3aa1646ba687711d6f54e2081c0aa840242c7b63
Micah Halter <micah@mehalter.com>
2024-09-16 03:28:19 -0400

    fix(util): use WCAG contrast ratio to compare colors

--- lua/todo-comments/util.lua
+++ lua/todo-comments/util.lua
@@ -14,22 +14,38 @@ function M.get_hl(name)
   return hl
 end
 
-function M.hex2rgb(hex)
+local function rgb_to_linear(c)
+  c = c / 255
+  return c <= 0.04045 and c / 12.92 or ((c + 0.055) / 1.055) ^ 2.4
+end
+
+local function relative_luminance(color)
+  return 0.2126 * color.r + 0.7152 * color.g + 0.0722 * color.b
+end
+
+function M.hex2linear_srgb(hex)
   hex = hex:gsub("#", "")
-  return tonumber("0x" .. hex:sub(1, 2)), tonumber("0x" .. hex:sub(3, 4)), tonumber("0x" .. hex:sub(5, 6))
+  return {
+    r = rgb_to_linear(tonumber("0x" .. hex:sub(1, 2))),
+    g = rgb_to_linear(tonumber("0x" .. hex:sub(3, 4))),
+    b = rgb_to_linear(tonumber("0x" .. hex:sub(5, 6))),
+  }
 end
 
-function M.rgb2hex(r, g, b)
-  return string.format("#%02x%02x%02x", r, g, b)
-end
+function M.contrast_ratio(c1, c2)
+  local lum1 = relative_luminance(c1)
+  local lum2 = relative_luminance(c2)
 
-function M.color_distance(r1, g1, b1, r2, g2, b2)
-  return math.sqrt((r1 - r2) ^ 2 + (g1 - g2) ^ 2 + (b1 - b2) ^ 2)
+  if lum1 < lum2 then
+    lum1, lum2 = lum2, lum1
+  end
+
+  return (lum1 + 0.05) / (lum2 + 0.05)
 end
 
 function M.maximize_contrast(base, fg1, fg2)
-  local r, g, b = M.hex2rgb(base)
-  return M.color_distance(r, g, b, M.hex2rgb(fg1)) > M.color_distance(r, g, b, M.hex2rgb(fg2)) and fg1 or fg2
+  base = M.hex2linear_srgb(base)
+  return M.contrast_ratio(base, M.hex2linear_srgb(fg1)) > M.contrast_ratio(base, M.hex2linear_srgb(fg2)) and fg1 or fg2
 end
 
 function M.warn(msg)
