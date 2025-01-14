304a8d204ee787d2544d8bc23cd38d2f929e7cc5
Folke Lemaitre <folke.lemaitre@gmail.com>
2025-01-14 17:50:09 +0100

    fix(snacks): snacks picker has not been released yet...

--- lua/todo-comments/config.lua
+++ lua/todo-comments/config.lua
@@ -133,7 +133,7 @@ function M._setup()
   M.colors()
   M.signs()
   require("todo-comments.highlight").start()
-  if Snacks then
+  if Snacks and pcall(require, "snacks.picker") then
     Snacks.picker.sources.todo_comments = require("todo-comments.snacks").source
   end
   M.loaded = true
